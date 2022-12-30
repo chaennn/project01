49b00395db342e3c9d8b3e906c4e04ede2c1b8c9
chaennn <girmn22@gmail.com>
2022-12-30 17:51:44 +0900

    모바일 메뉴 수정중

--- js/common.js
+++ js/common.js
@@ -1,3 +1,27 @@
+const $btnHam = document.querySelector(".menu_btn");
+const $nav = document.querySelector("nav");
+$btnHam.addEventListener("click", function () {
+  $nav.classList.toggle("on");
+  $btnHam.classList.toggle("on");
+});
+
+const $gnbLi = document.querySelectorAll(".gnb > li");
+const $sub = document.querySelectorAll(".sub");
+
+$gnbLi.addEventListener("click", function () {
+  $gnbLi.classList.toggle("on");
+  $sub.classList.add("on");
+
+  // $gnbLi.forEach(function (item, idx) {
+  //   item.addEventListener("click", function (e) {
+  //     e.preventDefault();
+  //     $sub.forEach(function (sub) {
+  //       sub.style.display = "none";
+  //     });
+  //     $sub[idx].style.display = "block";
+  //   });
+});
+
 console.log(
   `
     :::    :::  ::::::::::  :::         :::          ::::::::  
